6f2bd22dd0d8914717e488138320d26004cd4d93
jhooonds <jonatan_ds@msn.com>
2019-08-16 11:28:43 -0300

    alterações como diretivas e services criadas ou alteradas

---
+++ src/app/conversor/services/conversor.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Conversao, ConversaoResponse } from '../models';
+
+@Injectable()
+export class ConversorService {
+  private readonly BASE_URL = "http://api.fixer.io/latest"
+    
+
+  constructor(private http: HttpClient) {}
+
+  /**
+   * Realiza uma chamada para a API de conversão de moedas
+   *
+   * @param Conversao Conversao
+   * @return Observable <any>
+   */
+  converter(conversao: Conversao): Observable<any> {
+    let params = `?base=${conversao.moedaDe}&symbols=${conversao.moedaPara}`;
+    return this.http.get(this.BASE_URL + params);
+  }
+
+  /**
+   * Retorna a cotação para dado uma response.
+   *
+   * @param ConversaoResponse conversaoResponse
+   * @param Conversao conversao
+   * @return number
+   */
+  cotacaoPara(
+    conversaoResponse: ConversaoResponse,
+    conversao: Conversao
+  ): number {
+    if (conversaoResponse === undefined) {
+      return 0;
+    }
+    return conversaoResponse.rates[conversao.moedaPara];
+  }
+
+  /**
+   * Retorna a cotação de dado uma response.
+   *
+   * @param ConversaoResponse conversaoResponse
+   * @param Conversao conversao
+   * @return string
+   */
+  cotacaoDe(
+    conversaoResponse: ConversaoResponse,
+    conversao: Conversao
+  ): string {
+    if (conversaoResponse === undefined) {
+      return '0';
+    }
+    return (1 / conversaoResponse.rates[conversao.moedaPara]).toFixed(4);
+  }
+
+  /**
+   * Retorna a data da cotação dado uma response.
+   *
+   * @param ConversaoResponse conversaoResponse
+   * @return string
+   */
+  dataCotacao(conversaoResponse: ConversaoResponse): string {
+    if (conversaoResponse === undefined) {
+      return '';
+    }
+    return conversaoResponse.date;
+  }
+}
